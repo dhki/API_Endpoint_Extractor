5fa72bb319f566acd2271ed2c48c8ce11c797c5a
dhki <kdh101800@gmail.com>
2025-08-23 21:25:15 +0900

    feat: error handling (file exist)

--- utils/xml_parse.py
+++ utils/xml_parse.py
@@ -1,8 +1,14 @@
+import os
 import xml.etree.ElementTree as ET
 
 ## parse xml file and return urls
 ## if component's value starts with http/https, consider it as API Endpoints
 def parse_xml_values(path: str):
+    if os.path.exists(path) is False:
+        print("[!] File does not exist")
+        print(f"    {path}")
+        return []
+
     ret = []
     tree = ET.parse(path)
     root = tree.getroot()
