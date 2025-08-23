3513e9d7dbc758b049801cbed3ffad5c0a5b3f47
dhki <kdh101800@gmail.com>
2025-08-23 11:13:10 +0900

    feat: parsing string.xml

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import os
 import argparse
 import subprocess
 import shutil
 
 from utils.result_writer import ResultWriter
+from utils.xml_parse import parse_xml_values
 
 DECOMPILED_SMALI_PATH = "./target_smali"
+
+api_endpoints = set()
+smali_variables : dict = {}
 
 # decompile apk with apktool
 # if apktool is not exist in PATH, error occur.
@@ -21,6 +26,7 @@ def decompile_apk(apk_file: str):
 
     try:
         subprocess.run(cmd, shell=True)
+
         print("[+] Decompile completed")
     except Exception as e:
         print("[!] Error occured")
@@ -31,12 +37,23 @@ def main():
     # parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('apk_path', type=str, help='path of target apk file')
+    parser.add_argument("-o", "--output_file", type=str, default=None)
     args = parser.parse_args()
+
+    result_writer = ResultWriter(path=args.output_file)
 
     # decompile apk
     decompile_apk(apk_file=args.apk_path)
 
-    ## TODO: Find API Endpoint
+    # parse strings.xml
+    xml_urls = parse_xml_values(f"{DECOMPILED_SMALI_PATH}/res/values/strings.xml")
+    for url in xml_urls:
+        api_endpoints.add(url)
+    
+    # make result file (or print at console)
+    for v in api_endpoints:
+        result_writer.write(v)
+    result_writer.close()
 
 if __name__ == "__main__":
     main()
---
+++ utils/xml_parse.py
@@ -0,0 +1,15 @@
+import xml.etree.ElementTree as ET
+
+## parse xml file and return urls
+## if component's value starts with http/https, consider it as API Endpoints
+def parse_xml_values(path: str):
+    ret = []
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+    for elem in root.iter():
+        if elem.text and (elem.text.startswith("http://") or elem.text.startswith("https://")):
+           ret.append(elem.text.strip())
+    
+    return ret
+    
